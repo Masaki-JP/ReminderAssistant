9a8db5564f590b52edf4620d2d96b17b4a3d4265
Masaki-JP <masakidayo0508@gmail.com>
2026-08-31 00:04:31 +0900

    Initial Commit

---
+++ ReminderAssistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReminderAssistant
+//
+//  Created by Masaki Doi on 2026/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReminderAssistant/ReminderAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAssistantApp.swift
+//  ReminderAssistant
+//
+//  Created by Masaki Doi on 2026/08/31.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
