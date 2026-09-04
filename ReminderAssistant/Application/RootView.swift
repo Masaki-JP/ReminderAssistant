39c9fd1f4fa41745c29094a8bcae6e6f0793165e
Masaki-JP <masakidayo0508@gmail.com>
2026-09-04 16:09:27 +0900

    リマインダーのアクセス許可状態を表示するRootViewを追加

---
+++ ReminderAssistant/Application/RootView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+import EventKit
+
+struct RootView: View {
+    @State private var isReminderAccessGranted = EKEventStore.authorizationStatus(for: .reminder) == .fullAccess
+    @Environment(\.scenePhase) private var scenePhase: ScenePhase
+    
+    var isPreview: Bool {
+        ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+    }
+    
+    var body: some View {
+        Group {
+            if isReminderAccessGranted == true {
+                ContentView(onReminderAccessRevoked: { isReminderAccessGranted = false })
+            } else {
+                ReminderAccessRequestView(onReminderAccessGranted: { isReminderAccessGranted = true })
+            }
+        }
+        .onChange(of: scenePhase) { _, newValue in
+            guard newValue == .active else { return }
+            
+            if isPreview == true {
+                isReminderAccessGranted = false
+            } else {
+                isReminderAccessGranted = EKEventStore.authorizationStatus(for: .reminder) == .fullAccess
+            }
+        }
+    }
+}
+
+#Preview("Light") { RootView().preferredColorScheme(.light) }
+#Preview("Dark") { RootView().preferredColorScheme(.dark) }
