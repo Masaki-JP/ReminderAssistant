10132c474a6d944f5008dc964893325e29d5243f
Masaki-JP <masakidayo0508@gmail.com>
2026-09-08 03:00:50 +0900

    リマインダーの期限をグレゴリオ暦で生成

--- ReminderAssistant/Features/Reminders/Data/FakeReminderStore.swift
+++ ReminderAssistant/Features/Reminders/Data/FakeReminderStore.swift
@@ -102,8 +102,9 @@ actor FakeReminderStore: ReminderStoreProtocol {
                 )
             }
             
+            let dueDateCalendar = Calendar.gregorianCalendar()
             let dueDate = JapaneseDateConverter().convert(from: request.deadline).map {
-                Calendar.current.dateComponents([.year, .month, .day, .hour, .minute], from: $0)
+                dueDateCalendar.dateComponents([.year, .month, .day, .hour, .minute], from: $0)
             }
             
             try checkCancel()
