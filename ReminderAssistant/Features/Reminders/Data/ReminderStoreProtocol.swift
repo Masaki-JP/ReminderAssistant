91ff52e795adf24f9b43ebe8c7ffabfe55bdf478
Masaki-JP <masakidayo0508@gmail.com>
2026-09-11 05:26:46 +0900

    エラー型の定義場所を整理

--- ReminderAssistant/Features/Reminders/Data/ReminderStoreProtocol.swift
+++ ReminderAssistant/Features/Reminders/Data/ReminderStoreProtocol.swift
@@ -1,15 +1,5 @@
 import Foundation
 
-enum ReminderStoreError: Error, Equatable {
-    case accessNotAuthorized
-    case listNotFound(calendarIdentifier: String)
-    case reminderNotFound(calendarItemIdentifier: String)
-    case fetchFailed
-    case saveFailed
-    case cancelled
-    case deadlineConversionFailed
-}
-
 protocol ReminderStoreProtocol: Actor {
     nonisolated var remindersMayHaveChangedNotification: Notification.Name { get }
     
@@ -24,4 +14,14 @@ protocol ReminderStoreProtocol: Actor {
     func set(id: String, completion: Bool) async throws(ReminderStoreError)
     
     func fetch() async throws(ReminderStoreError) -> [ReminderList]
+}
+
+enum ReminderStoreError: Error, Equatable {
+    case accessNotAuthorized
+    case listNotFound(calendarIdentifier: String)
+    case reminderNotFound(calendarItemIdentifier: String)
+    case fetchFailed
+    case saveFailed
+    case cancelled
+    case deadlineConversionFailed
 }
